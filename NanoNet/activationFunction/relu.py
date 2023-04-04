fixed overflow error in softmax

NanoNet/activationFunction/relu.py
@@ -2,9 +2,13 @@ import numpy as np
 
 
 class ReLu:
-
+    
+    @staticmethod
     def forward(z):
         return np.maximum(0, z)
     
+    @staticmethod
     def derivative(z):
-        return np.where(z<=0, 0, 1)
+        z[z<=0] = 0
+        z[z>0] = 1
+        return z

NanoNet/activationFunction/softmax.py
@@ -4,11 +4,16 @@ class SoftMax():
 
     @staticmethod
     def forward(z):
+        if z.ndim == 1:
+            exp_values = np.exp(z- np.max(z))
+            probabilities = exp_values / np.sum(exp_values)
+            if np.sum(exp_values) == 0:
+                raise KeyboardInterrupt
+        else:
 
-        exp_values = np.exp(z)
-
-        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
-
+            exp_values = np.exp(z- np.max(z, axis=1, keepdims=True))
+            probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True) 
+        
         return probabilities
     
 

NanoNet/costFunction/costFunction.py
@@ -9,27 +9,39 @@ class CostFunction:
     # l2, l1 regularisation
 
 class CrossEntropy(CostFunction):
-    
-    def __init__(self, l1 : bool, l2 : bool):
-        pass
-    @staticmethod
-    def forward():
-        pass
 
     @staticmethod
-    def delta():
-        pass
+    def forward(a, y):
+        """Return the cost associated with an output ``a`` and desired output
+        ``y``.  Note that np.nan_to_num is used to ensure numerical
+        stability.  In particular, if both ``a`` and ``y`` have a 1.0
+        in the same slot, then the expression (1-y)*np.log(1-a)
+        returns nan.  The np.nan_to_num ensures that that is converted
+        to the correct value (0.0).
 
+        """
+        return np.sum(np.nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a)))
+
+    @staticmethod
+    def delta(z, a, y, activation=None):
+        """Return the error delta from the output layer.  Note that the
+        parameter ``z`` is not used by the method.  It is included in
+        the method's parameters in order to make the interface
+        consistent with the delta method for other cost classes.
+
+        """
+        return (a-y) # only with sigmoid otherwise division through zero might occure
 
 class LogLikelihood(CostFunction):
 
     @staticmethod
-    def forward():
-        pass
+    def forward(a, y):
+        index = y.argmax(y)
+        return np.log(a[index])
 
     @staticmethod
-    def delta():
-        pass
+    def delta(z, a, y, activation=None):
+        return (a-y) # only when last layer uses softmax
 
 
 class QuadraticCost(CostFunction):
@@ -39,6 +51,6 @@ class QuadraticCost(CostFunction):
         return 0.5*np.linalg.norm(a-y)**2
 
     @staticmethod
-    def delta(z, a, y):
+    def delta(z, a, y, activation):
         """Return the error delta from the output layer."""
-        return (a-y) * Sigmoid().derivative(z)
+        return (a-y)*activation.derivative(z)

NanoNet/network.py
@@ -4,6 +4,7 @@ import json
 import sys
 import time
 
+
 class Network:
 
     def __init__(self, sizes: list, optimizer: object, a_functions: list, cost_function: object, test_data: list = None, training_data:list = None, w_init_size: str ="small"):
@@ -20,9 +21,9 @@ class Network:
 
     def initialize_weights(self, size):
         if size.lower() == "small":
-            return [np.random.randn(x, y)/np.sqrt(x) for x, y in zip(self.sizes[:-1], self.sizes[1:])]
+            return [0.01*np.random.randn(x, y)/np.sqrt(x) for x, y in zip(self.sizes[:-1], self.sizes[1:])]
         elif size.lower() == "large":
-            return [0.1*np.random.randn(x, y) for x, y in zip(self.sizes[:-1], self.sizes[1:])]
+            return [0.01*np.random.randn(x, y) for x, y in zip(self.sizes[:-1], self.sizes[1:])]
 
     def initialize_activations(self, a):
         if len(a) == self.num_layers - 1:
@@ -49,8 +50,11 @@ class Network:
 
     def feedforward(self, a):
         joined = list(zip(self.biases, self.weights))
+        
         for i in range(0, self.num_layers-1):
             a = self.a_functions[i].forward(np.dot(joined[i][1].T, a).T+joined[i][0])
+
+        #print(a)
         return a
     
     def evaluate(self):
@@ -74,6 +78,8 @@ class Network:
 
             self.weights, self.biases = self.optimizer.WEIGHTS, self.optimizer.BIASES
 
+
+        
             if self.test_data:
                 print(f"Epoch {j+1}: {self.evaluate()} / {n_test}")
             else:

NanoNet/optimizer/optimizer.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import sys
 
 
 class Optimizer:
@@ -24,7 +25,7 @@ class SGD(Optimizer):
 
         n = len(trainig_data)
 
-        random.shuffle(trainig_data)
+        #random.shuffle(trainig_data)
 
         mini_batches = []
         controll = []
@@ -40,8 +41,9 @@ class SGD(Optimizer):
                 
         data = zip(mini_batches, controll)
         for mini_batch, controll in data:
+            #print(mini_batch)
             #print(np.array(controll).shape)
-            self.update_mini_batch(mini_batch, controll, self.eta)
+            self.update_mini_batch(mini_batch, controll)
 
     def backprop(self, x, y):
         nabla_b = [np.zeros(b.shape) for b in self.BIASES]
@@ -54,11 +56,13 @@ class SGD(Optimizer):
         joined = list(zip(self.BIASES,self.WEIGHTS))
         for i in range(0, self.NUM_LAYERS-1):
             z = np.dot(activations[-1], joined[i][1]) + joined[i][0]
+            
+
             zs.append(z)
             activation = self.ACTIVATION_FUNCTIONS[i].forward(z)
             activations.append(activation)
 
-        delta = self.COST_FUNCTION.delta(zs[-1], activation[-1], y)
+        delta = self.COST_FUNCTION.delta(zs[-1], activation[-1], y, self.ACTIVATION_FUNCTIONS[-1])
         nabla_b[-1] = delta.mean(0)
         nabla_w[-1] = np.dot(delta.T, activations[-2]).T / len(y)
 
@@ -67,17 +71,23 @@ class SGD(Optimizer):
             z = zs[-l]
             sp = self.ACTIVATION_FUNCTIONS[-l].derivative(z)
             delta = np.dot(self.WEIGHTS[-l+1], delta.T).T * sp
+            if np.isnan(sp).all():
+                print(sp)
+                #print(z)
+                raise KeyboardInterrupt
             nabla_b[-l] = delta.mean(0)
             #print((self.biases[-l]-nabla_b[-l]).shape)
             nabla_w[-l] = np.dot(delta.T, activations[-l-1]).T / len(y)
         return (nabla_b, nabla_w)
 
-    def update_mini_batch(self, mini_batch, controll, eta):
+    def update_mini_batch(self, mini_batch, controll):
 
         delta_nabla_b, delta_nabla_w = self.backprop(mini_batch, controll)
 
-        self.WEIGHTS = [w-eta*nw
+        self.WEIGHTS = [w-self.eta*nw
                         for w, nw in zip(self.WEIGHTS, delta_nabla_w)]
-        self.BIASES  = [b-eta*nb
+        self.BIASES  = [b-self.eta*nb
                         for b, nb in zip(self.BIASES, delta_nabla_b)]
         
+        #print(delta_nabla_b)
+        #print(self.WEIGHTS)