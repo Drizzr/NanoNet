0278e05cab9be954710b44beced7217f159184d5
Drizzr <samuelfriese@gmx.net>
2023-04-10 19:10:28 +0200

    adam optimizer implemented

--- NanoNet/optimizer/adam.py
+++ NanoNet/optimizer/adam.py
@@ -1,5 +1,45 @@
 from .rms_promp import RMSPromp
 from .sgd_momentum import SGD_Momentum
+import numpy as np
 
 class ADAM(SGD_Momentum, RMSPromp):
-    pass
+    vel_w = None
+    vel_b = None
+    estimated_crude_variance_w = None
+    estimated_crude_variance_b = None
+    EPSILON = 10e-7
+
+    def __init__(self, eta, beta1=0.9, beta2=0.99):
+        self.eta = eta
+        self.beta1 = beta1
+        self.beta2 = beta2
+    
+    def minimize(self, trainig_data):
+
+        #random.shuffle(trainig_data)
+                
+        data = self.create_minibatch(trainig_data)
+        for mini_batch, controll in data:
+            self.update_mini_batch(mini_batch, controll)
+    
+
+    def update_mini_batch(self, mini_batch, controll):
+        if not self.estimated_crude_variance_w:
+            self.estimated_crude_variance_b = [np.zeros(b.shape) for b in self.BIASES]
+            self.estimated_crude_variance_w = [np.zeros(w.shape) for w in self.WEIGHTS]
+            self.vel_b = [np.zeros(b.shape) for b in self.BIASES]
+            self.vel_w = [np.zeros(w.shape) for w in self.WEIGHTS]
+
+        delta_nabla_b, delta_nabla_w = self.backprop(mini_batch, controll)
+    
+        self.estimated_crude_variance_w = [self.beta2*crude+(1-self.beta2)*nw**2 for crude, nw in zip(self.estimated_crude_variance_w, delta_nabla_w)]
+        self.estimated_crude_variance_b = [self.beta2*crude+(1-self.beta2)*nb**2 for crude, nb in zip(self.estimated_crude_variance_b, delta_nabla_b)]
+        self.vel_w = [self.beta1*vel_w+(1-self.beta1)*nw for vel_w, nw in zip(self.vel_w, delta_nabla_w)]
+        self.vel_b = [self.beta1*vel_b+(1-self.beta1)*nb for vel_b, nb in zip(self.vel_b, delta_nabla_b)]
+
+
+        self.WEIGHTS = [w-self.eta*(1/np.sqrt(crude + self.EPSILON))*vel_w
+                        for w, crude, vel_w in zip(self.WEIGHTS, self.estimated_crude_variance_w, self.vel_w)]
+        
+        self.BIASES = [b-self.eta*(1/np.sqrt(crude + self.EPSILON))*vel_b
+                        for b, crude, vel_b in zip(self.BIASES, self.estimated_crude_variance_b, self.vel_b)]
--- mnis_example.py
+++ mnis_example.py
@@ -1,10 +1,10 @@
 from NanoNet.network import Network
-from NanoNet.optimizer import SGD, SGD_Momentum, RMSPromp
+from NanoNet.optimizer import SGD, SGD_Momentum, RMSPromp, ADAM
 from NanoNet.costFunction import QuadraticCost, CrossEntropy, LogLikelihood
 from NanoNet.activationFunction import Sigmoid, ReLu, SoftMax
 from mnis_loadaer import load_data_wrapper
 
 training_data, validation_data, test_data = load_data_wrapper()
 
-net = Network([784, 100, 20, 10], 10, RMSPromp(0.0002),[ReLu(), ReLu(), SoftMax()], LogLikelihood(), test_data=test_data, training_data=training_data, w_init_size="large")
+net = Network([784, 100, 20, 10], 10, ADAM(0.0002),[ReLu(), ReLu(), SoftMax()], LogLikelihood(), test_data=test_data, training_data=training_data, w_init_size="large")
 net.train(40, monitor_training_cost=False, monitor_training_accuracy=False, monitor_test_cost=True, monitor_test_accuracy=True)
