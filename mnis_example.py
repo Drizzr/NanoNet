410f5b0f7e04f0a0ca5906d51fa412ab5b2441be
Drizzr <samuelfriese@gmx.net>
2023-04-11 14:02:22 +0200

    added licence

--- mnis_example.py
+++ mnis_example.py
@@ -6,5 +6,5 @@ from mnis_loadaer import load_data_wrapper
 
 training_data, validation_data, test_data = load_data_wrapper()
 
-net = Network([784, 100, 20, 10], 10, ADAM(0.0002),[ReLu(), ReLu(), SoftMax()], LogLikelihood(), test_data=test_data, training_data=training_data, w_init_size="large")
+net = Network([784, 100, 20, 10], 10, ADAM(0.002),[Sigmoid(), ReLu(), SoftMax()], LogLikelihood(), test_data=test_data, training_data=training_data, w_init_size="large")
 net.train(40, monitor_training_cost=False, monitor_training_accuracy=False, monitor_test_cost=True, monitor_test_accuracy=True)
